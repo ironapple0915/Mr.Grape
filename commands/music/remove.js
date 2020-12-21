799eca9cc854cec776e8da81e1179f55ceb7768f
ironapple0915 <67236689+ironapple0915@users.noreply.github.com>
2020-12-21 08:50:54 -0500

    Update remove.js

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -18,7 +18,7 @@ module.exports = {
                 .setTitle('Song Queue')
                 .addField(`Removed **${arr[remove].title}**`, '_')
                 .setTimestamp()
-                .setFooter('DJ Grape');
+                .setFooter('night');
             message.channel.send(rm)
             if (remove === 0) { skip.execute(message, args, d); }
             else { arr.splice(remove, 1); }
