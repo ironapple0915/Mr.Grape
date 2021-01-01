7b1f44a9b4a86bf5135c6a1e1707712b7aae23ec
ironapple0915 <67236689+ironapple0915@users.noreply.github.com>
2021-01-01 00:34:16 -0500

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,3 +1,9 @@
+const Discord = require('ms');
+module.exports = {
+    name: 'mute',
+    description: 'Mutes a user',
+    cooldown: 0,
+    
 const mutedRole = message.guild.roles.cache.get('<Muted Role ID>');
 
 // or, if you can't get the id:
@@ -8,3 +14,8 @@ const mutedRole = message.guild.roles.cache.find(
 // if there is no `Muted` role, send an error
 if (!mutedRole)
  return message.channel.send('There is no Muted role on this server');
+     setTimeout(() => {
+  target.roles.remove(mutedRole); // remove the role
+}, ms(args[1])) 
+};
+
