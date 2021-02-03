cbf5c9cab6a49c58681eb34b0985470aab3a8a94
ironapple0915 <67236689+ironapple0915@users.noreply.github.com>
2021-02-03 17:23:45 -0500

    Create embed.js

---
+++ commands/economy/embed.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "embed",
+    aliases: ['embed'],
+    description: 'idk',
+    cooldown: o,
+    cd: "It's not an addiction I swear.",
+    fan: true,
+    async execute(message, args, d) {  
+    message.channel.send("you are an idiot. Give a valid number.")
+            return
+            }
