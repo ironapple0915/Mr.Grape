dd9cab7bcda774fb5d324c434c1e877a7ea4a5cd
ironapple0915 <67236689+ironapple0915@users.noreply.github.com>
2021-02-03 17:54:17 -0500

    Update embed.js

--- commands/economy/embed.js
+++ commands/economy/embed.js
@@ -5,7 +5,7 @@
     cooldown: 0,
     cd: "It's not an addiction I swear.",
     fan: true,
-    async execute(message, args, d) {  
+      
     message.channel.send("you are an idiot. Give a valid number.")
             return
             }}
