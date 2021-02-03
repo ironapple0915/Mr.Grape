c7b2512d356c9a7a8deb76cb0a9ea325c51ca93a
ironapple0915 <67236689+ironapple0915@users.noreply.github.com>
2021-02-03 17:59:58 -0500

    Update embed.js

--- commands/economy/embed.js
+++ commands/economy/embed.js
@@ -1,8 +1,6 @@
    module.exports = {
     name: "embed"
-    cooldown: 0,
-    fan: true,
       
     message.channel.send("you are an idiot. Give a valid number.")
             return
-            }}
+            }
